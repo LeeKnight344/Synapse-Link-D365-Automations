621c2605b6c16c42b59aa8a49520864e6d855f01
Lee Knight <leeknight352@outlook.com>
2025-06-25 11:23:23 +0100

    Add IIS Publish task to setup script

--- SynapseLinkAutomations.Host/modules/downloadandbuild.ps1
+++ SynapseLinkAutomations.Host/modules/downloadandbuild.ps1
@@ -1,8 +1,18 @@
+# Download and extract the repo
 $repoUrl = "https://codeload.github.com/LeeKnight344/Synapse-Link-D365-Automations/zip/240a173126ef6965c523f5e996ed6a41e23528f2"
 $zipPath = "$env:TEMP\repo.zip"
 $destPath = "C:\SynapseAutomation"
+$webRoot = "C:\inetpub\wwwroot"
+
 Invoke-WebRequest -Uri $repoUrl -OutFile $zipPath
 Expand-Archive -Path $zipPath -DestinationPath $destPath -Force
-Set-Location "$destPath\Synapse-Link-D365-Automations-240a173126ef6965c523f5e996ed6a41e23528f2"
-dotnet publish Synapse-Link-D365-Automations.sln
-cd "c:\SynapseAutomation\Synapse-Link-D365-Automations-240a173126ef6965c523f5e996ed6a41e23528f2\SynapseLinkAutomations\bin\Release\net8.0"
+
+# Navigate to the repo
+$repoRoot = "$destPath\Synapse-Link-D365-Automations-240a173126ef6965c523f5e996ed6a41e23528f2"
+Set-Location $repoRoot
+
+$publishPath = "$destPath\publish"
+dotnet publish Synapse-Link-D365-Automations.sln -c Release -o $publishPath
+New-Item -ItemType Directory -Force -Path $webRoot | Out-Null
+Copy-Item -Path "$publishPath\*" -Destination $webRoot -Recurse -Force
+iisreset
